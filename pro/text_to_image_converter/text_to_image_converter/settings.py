26afe3a1304e384c5e6d5e3c82a992b25020da1e
Sai Chandu <99642415+kscm952@users.noreply.github.com>
2024-03-25 10:33:20 +0530

    Update settings.py

--- pro/text_to_image_converter/text_to_image_converter/settings.py
+++ pro/text_to_image_converter/text_to_image_converter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$53kaj^61red&$ybitkm5$i(gie$un3xg@k^woz!pz83drj_&x'
+SECRET_KEY = 'django-insecure-$53kaj^61red&$ybitkm5$i(gie$un3xg@k^woz!pz83drj_&x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
