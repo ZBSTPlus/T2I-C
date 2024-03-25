f40d72b6009cf5ecc42670533bf8565f55854ba6
Sai Chandu <99642415+kscm952@users.noreply.github.com>
2024-03-25 10:34:17 +0530

    Delete pro/text_to_image_converter directory

--- pro/text_to_image_converter/text_converter/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TextToImage
-
-class TextToImageForm(forms.ModelForm):
-    class Meta:
-        model = TextToImage
-        fields = ['text', 'font_size']
--- pro/text_to_image_converter/text_converter/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from text_converter.views import text_to_image
-
-urlpatterns = [
-    path('text-to-image/', text_to_image, name='text_to_image'),
-]
